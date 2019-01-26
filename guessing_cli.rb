bc9b88706c4fca423404d93e33ad2dfcdab9bddc
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-26 17:40:12 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,8 +1,10 @@
+require "pry"
 def run_guessing_game
   while true
     answer = rand(1..6)
     puts "Guess a number between 1 and 6."
     guess = gets.chomp
+    binding.pry
     case guess
     when answer
       puts "You guessed the correct number!"
